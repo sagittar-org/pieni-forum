4a28c5f35ef9cfb6f0f9483eedb17922e686657f
Kenta Saito <kenta.saito@sagittar.org>
2017-08-28 17:37:30 +0900

    2017-08-28 17:37:30

---
+++ language/common.php
@@ -0,0 +1,10 @@
+<?php
+$language['post']                  = 'Forum';
+$language['member_post']           = 'Post';
+$language['post_id']               = 'Post ID';
+$language['post_member_id']        = 'Member ID';
+$language['post_name']             = 'Title';
+$language['post_created']          = 'Posted at';
+$language['post_text']             = 'Text';
+$language['post_image']            = 'Image';
+$language['count_comment']         = 'Comments';
