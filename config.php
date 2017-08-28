0a555adcef9143682f166e466cbe270bd10cbf0d
Kenta Saito <kenta.saito@sagittar.org>
2017-08-28 16:27:07 +0900

    2017-08-28 16:27:07

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
-$config['uri']['actor_hash'] = [
-	'g' => 'guest',
-	'm' => 'member',
-	'a' => 'admin',
+$config['uri']['class_list'] = [
+	'welcome',
+	'inquiry',
+	'auth',
+	'spec',
+	'docs',
+	'forum',
 ];
 $config['uri']['table_list'] = [
 	'post',
@@ -11,41 +14,18 @@ $config['uri']['table_list'] = [
 	'directive',
 ];
 $config['uri']['action_hash'] = [
-	'proxy'  => 'row',
-	'index'  => 'index',
-	'view'   => 'view',
-	'reply'  => 'edit',
-	'add'    => 'add',
-	'edit'   => 'edit',
-	'delete' => 'delete',
+	'proxy'    => 'row',
+	'index'    => 'index',
+	'view'     => 'view',
+	'reply'    => 'edit',
+	'add'      => 'add',
+	'edit'     => 'edit',
+	'delete'   => 'delete',
 ];
 $config['uri']['alias_list'] = [
 	'post_post',
 ];
-$config['auth'] = [
-	'm' => [
-		'join'  => 'INSERT INTO `member` (`member_name`, `member_email`, `member_password`) values ("$name", "$email", "$password")',
-		'login' => 'SELECT `member_id` AS `id`, `member_name` AS `name`, `member_email` AS `email`, `member_password` AS `password` FROM `member` WHERE `member_email` = "$1"',
-		'proxy' => 'SELECT `member_id` AS `id`, `member_name` AS `name`, `member_email` AS `email` FROM `member` WHERE `member_id` = "$1"',
-	],
-	'a' => [
-		'login' => 'SELECT `admin_id` AS `id`, `admin_name` AS `name`, `admin_email` AS `email`, `admin_password` AS `password` FROM `admin` WHERE `admin_email` = "$1"',
-	],
-];
-$config['db'] = [
-	'debug'    => FALSE,
-	'host'     => 'localhost',
-	'username' => 'root',
-	'passwd'   => '',
-	'dbname'   => 'pieni-forum',
-];
-$config['mail'] = [
-	'from'     => 'noreply@localhost',
-	'admin'    => 'root@localhost',
-];
-$config['session'] = [
-	'name'     => 'pieni-forum',
-];
-$config['password'] = [
-	'hash'     => TRUE,
-];
+
+$config['db']['dbname'] = 'pieni-forum';
+
+$config['session']['name'] = 'pieni-forum';
